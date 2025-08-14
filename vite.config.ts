7ec9979aaa79f090c55b1a8580440669308e64e7
ZacharyFinley <zacharyfinley@gmail.com>
2025-08-14 14:41:39 -0400

    Update base for build url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 
-// NOTE: if deploying to GitHub Pages at https://<user>.github.io/<repo>/,
-// set base: '/<repo>/' before building.
 export default defineConfig({
   server: { port: 5173 },
   build: { outDir: 'dist', sourcemap: false },
-  base: 'https://github.com/ZacharyFinley/BattleSim.git',
+  base: '/BattleSim/',
 });
